6336be1ab2bc3689b64aaf70efece91bbdc2d37a
SerikovZhalgas <zhalgasserikov@gmail.com>
2022-06-27 00:32:09 +0600

    01-03

---
+++ src/02/02_02.ts
@@ -0,0 +1,31 @@
+
+
+export type StreetType = {
+    title: string
+}
+
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+export type HousesType = {
+    buildedAt: number
+    repaired: false
+    address: AddressType
+}
+
+export type GovermentBuildingType = {
+    type: "HOSPITAL" | "FIRE-STATION"
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HousesType>
+    govermentBuilding: Array<GovermentBuildingType>
+    citizensNumber: number
+}
+
